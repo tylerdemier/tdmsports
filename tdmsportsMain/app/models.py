a0534b3044dc7594e33c945090b15452c7bf3112
Tyler de Mier <tylerdemier@opendeusto.es>
2023-01-09 06:20:37 +0100

    Flask terminado, quedan: cambios menores, modificaciones estéticas y API Gateway

--- tdmsportsMain/app/models.py
+++ tdmsportsMain/app/models.py
@@ -1,7 +1,7 @@
 from app import db
 from datetime import datetime
 import re
-
+import json
 
 # Creates a function to create a URL readable for a human.
 def slugify(string):
@@ -31,12 +31,27 @@ class Stock(db.Model):
         return f'<Stock id: {self.id}, code: {self.code}, name: {self.name}>'
 
 
+class JsonEncodedDict(db.TypeDecorator):
+    impl = db.Text
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return '{}'
+        else:
+            return json.dumps(value)
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return {}
+        else:
+            return json.loads(value)
+
+
+
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     invoice = db.Column(db.String(20), unique=True, nullable=False)
     status = db.Column(db.String(20), default='Pending', nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    orders = db.Column(db.Text)
+    orders = db.Column(JsonEncodedDict)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- tdmsportsMain/app/views.py
+++ tdmsportsMain/app/views.py
@@ -1,26 +1,88 @@
-from flask import render_template, request, session, redirect, url_for
+from IPython.core.display import Math
+from flask import render_template, request, session, redirect, url_for, make_response
 from app import app
 from models import Stock
+from models import Order
+from app import db
 
 import random
+import secrets
+# pdfkit: pdf creation
+import pdfkit
 # stripe: payment API
 import stripe
 
 publishable_key = 'pk_test_51MOA4RLMQNcaxruGmWcR9wPh0wZJEnDl5ablQms8AQsCkQNmNBBOyq6QpgfRUGFtQ4Df9hdbcqCEZWpvnKFkBYiY00TYzmiKBT'
 stripe.api_key = 'sk_test_51MOA4RLMQNcaxruGjTa93BUR24Ub2ZtSLzxggh7LcC8n3SyNF2H9drShK9kSVdLYcAV5IDbtVtNthqHhL7KqxMKt00PYbhtNA7'
 
-@app.route('/payment', methods=['POST'])
+random_shipping = round(random.uniform(1.99, 10.99), 2)
+
+
+@app.route('/payment', methods=['GET', 'POST'])
 def payment():
+    amount = request.form.get('amount')
+    amount_float = float(amount)
+    amount_round = int(round(amount_float, 2))
+    customer = stripe.Customer.create(
+        email=request.form['stripeEmail'],
+        source=request.form['stripeToken'],
+    )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        description='TDM Sports',
+        amount=amount_round,
+        currency='eur',
+    )
+
     return redirect(url_for('thanks'))
+
 
 @app.route('/thanks')
 def thanks():
-    return render_template('thanks.html')
+    invoice = secrets.token_hex(5)
+    order = Order(invoice=invoice, orders=session['shoppingcart'])
+    db.session.add(order)
+    db.session.commit()
+
+    for key, item in order.orders.items():
+        item_to_use = Stock.query.get(item['id'])
+        if item_to_use.amount > 1:
+            amount = int(item_to_use.amount)
+            item_to_use.amount = amount - 1
+        else:
+            db.session.delete(item_to_use)
+
+    db.session.commit()
+    session.pop('shoppingcart')
+
+    return render_template('thanks.html', order=order)
+
+
+@app.route('/get_order_pdf/<invoice>', methods=['POST'])
+def get_order_pdf(invoice):
+    grandtotal = 0
+    shipping = random_shipping
+    subtotal = 0
+    if request.method == "POST":
+        order = Order.query.filter(Order.invoice == invoice).first()
+        for key, item in order.orders.items():
+            subtotal = subtotal + float(item['price']) * int(item['quantity'])
+
+        grandtotal = subtotal + shipping
+
+        rendered = render_template('pdf.html', subtotal=subtotal, shipping=shipping, grandtotal=grandtotal)
+        pdf = pdfkit.from_string(rendered, False)
+        response = make_response(pdf)
+        response.headers['content-Type'] = 'application/pdf'
+        response.headers['content-Disposition'] = 'attached; filename=' + invoice + '.pdf'
+        return response
+    return redirect(url_for('index()'))
+
 
 # @todo:
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # @todo: hacer que solo salgan de los que hay mas amount que 0
     search = request.args.get('search')
     if search:
         stocks = Stock.query.filter((Stock.name.contains(search) | Stock.desc.contains(search)) & Stock.amount > 0)
@@ -34,11 +96,13 @@ def stock_detail(url):
     stock = Stock.query.filter(Stock.url == url).first()
     return render_template('item.html', stock=stock)
 
+
 def merge_dicts(dict_1, dict_2):
     if isinstance(dict_1, dict) and isinstance(dict_2, dict):
         return dict(list(dict_1.items()) + list(dict_2.items()))
     else:
         return False
+
 
 @app.route('/add_to_cart', methods=['POST'])
 def add_to_cart():
@@ -46,8 +110,9 @@ def add_to_cart():
     stock_quantity = request.form.get('stock_quantity')
     stock = Stock.query.filter(Stock.id == stock_id).first()
 
-    dict_items = {stock_id: {'name': stock.name, 'code': stock.code, 'price': stock.price, 'quantity': stock.amount,
-                            'image': stock.image, 'desc': stock.desc}}
+    dict_items = {stock_id: {'id': stock.id, 'name': stock.name, 'code': stock.code, 'price': stock.price,
+                             'quantity': 1,
+                             'image': stock.image, 'desc': stock.desc}}
 
     if 'shoppingcart' in session:
         print(session['shoppingcart'])
@@ -61,18 +126,20 @@ def add_to_cart():
         session['shoppingcart'] = dict_items
         return redirect(url_for("index"))
 
+
 @app.route('/cart')
 def get_cart():
     if 'shoppingcart' not in session:
         return redirect(url_for("index"))
 
     subtotal = 0
-    shipping = round(random.uniform(1.99, 10.99), 2)
+    shipping = random_shipping
     grandtotal = 0.0
     for key, item in session['shoppingcart'].items():
         subtotal = subtotal + float(item['price']) * int(item['quantity'])
     grandtotal = subtotal + shipping
-    return render_template('cart.html', subtotal = subtotal, shipping = shipping, grandtotal = grandtotal)
+    return render_template('cart.html', subtotal=subtotal, shipping=shipping, grandtotal=grandtotal)
+
 
 @app.route('/delete_item/<int:id>')
 def delete_item(id):
